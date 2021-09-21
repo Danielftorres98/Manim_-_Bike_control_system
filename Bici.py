6a8b24f75485c9006f58ac23c52eca0067d9aeb4
lmcastanedame <lmcastanedame@unal.edu.co>
2021-09-21 15:31:19 -0500

    Update Bici.py

--- Bici.py
+++ Bici.py
@@ -3,93 +3,149 @@
 # Proyecto Final Control
 
 from big_ol_pile_of_manim_imports import *
-# L1 signals
+from scipy import signal as sp
 
 def Range(in_val,end_val,step=1):
     return list(np.arange(in_val,end_val+step,step))
 
 class Anim_1_1(GraphScene):
-    CONFIG = {
-        "y_max" : 2,
-        "y_min" : 0,
-        "x_max" : 1,
-        "x_min" : 0,
-        "y_tick_frequency" : 10,
-        "x_tick_frequency" : 10,
-        "graph_origin" : 0,
-        "y_axis_label": None, 
-        "x_axis_label": None,
-    }
     
     def construct(self):
-
-        for x in range(-7, 8):
-            for y in range(-5, 6):
-                dot = Dot( point = (x, y, 0) )
-                self.add(dot)
-
-        Rueda_bici_trasera = RoundedRectangle(
+        axis_config = {
+            "y_max" : 4,
+            "y_min" : -4,
+            "x_max" : 4,
+            "x_min" : -4,
+            }
+        
+        Rueda1 = Circle(
                      color = BLUE,
-                     height = 2.3,
-                     width = 0.4,
-                     corner_radius = 0.2
+                     radius = 1.2
             )
 
-        Rueda_bici_trasera.set_fill(BLUE, opacity=0.3)
+        Rueda2 = Circle(
+                     color = BLUE,
+                     radius = 1
+            )
 
-        Linea_vertical_bici_trasera = Line( 1.5*UP,DOWN )
-        Linea_vertical_bici_trasera.set_color( RED )
-
-        Linea_horizontal_bici_trasera = Line( 0.5*RIGHT, RIGHT ) 
-        Linea_horizontal_bici_trasera.set_color( RED )
-
-        Silla_bici_trasera = RoundedRectangle(
-                     color = GREY,
-                     height = 2.3,
-                     width = 0.4,
-                     corner_radius = 0.2
-        )
-
-        Silla_bici_trasera.rotate(PI/2)
-        Silla_bici_trasera.scale(0.25)
-        Silla_bici_trasera.set_fill(GREY, opacity=0.3)
-
-
-        Rueda_bici_trasera.scale(0.6)
-        Rueda_bici_trasera.move_to(DOWN)
-        Linea_vertical_bici_trasera.next_to( Rueda_bici_trasera, UP, buff=.01 )
-        Linea_horizontal_bici_trasera.move_to( Linea_vertical_bici_trasera.get_center() + 0.4*DOWN )
-        Silla_bici_trasera.next_to( Linea_vertical_bici_trasera, UP, buff=.01 )
-
-        Bici_trasera = VGroup( Linea_vertical_bici_trasera, Linea_horizontal_bici_trasera, Silla_bici_trasera, Rueda_bici_trasera )
-        Bici_trasera.move_to( 0.5*DOWN )
-
-
-
-        self.setup_axes1()
-        self.play( ShowCreation( Bici_trasera ), run_time = 0.1 )
-        self.wait(2)
+        Rueda1.set_fill(BLUE, opacity=0.3)
+        Rueda2.set_fill(BLUE, opacity=0.3)
         
+        Rueda1.move_to(1.5*DOWN + 2.6*LEFT)
+        Rueda2.move_to(1.6*DOWN + 1.3*RIGHT)
+
+        Linea_horizontal = Line( start=LEFT,end=0.5*RIGHT )
+        Linea_horizontal.set_color( RED )
+
+        Linea1 = Line( start=np.array([-1, 0, 0]),end=np.array([-0.7, -1.5, 0]) ) 
+        Linea1.set_color( RED )
+
+        Linea2 = Line( start=LEFT,end=1.3*DOWN + 2.3*LEFT ) 
+        Linea2.set_color( RED )
+
+        Linea3 = Line( start=1.5*DOWN + 0.7*LEFT,end=1.3*DOWN + 2.3*LEFT ) 
+        Linea3.set_color( RED )
+
+        Linea4 = Line( start=0.5*RIGHT,end=0.4*DOWN + 0.65*RIGHT ) 
+        Linea4.set_color( RED )
+
+        Linea5 = Line( start=0.4*DOWN + 0.65*RIGHT,end=1.5*DOWN + 0.7*LEFT ) 
+        Linea5.set_color( RED )
+
+        Linea6 = Line( start=0.4*UP + 0.35*RIGHT,end= 1.4*DOWN + 1.025*RIGHT )
+        Linea6.set_color( RED )
+
+        Lineas = VGroup( Linea_horizontal, Linea1, Linea2, Linea3, Linea4, Linea5, Linea6 )
+        Lineas.scale(1.2)
+
+        Bici = VGroup( Rueda1, Rueda2, Lineas)
+
+        self.play(ShowCreation(Bici),run_time = 2)
+        self.wait(2)
+
+
+class Anim_1_2(ThreeDScene):
+    
+    def construct(self):
+        axis_config = {
+            "y_max" : 0,
+            "y_min" : -4,
+            "x_max" : 4,
+            "x_min" : 0,
+            "z_max" : 3,
+            "z_min" : 0,
+            }
+        
+        axes = ThreeDAxes(**axis_config)
+        self.set_camera_orientation(phi = 80*DEGREES, theta = -40*DEGREES, distance = 6)
+
+        Rueda1 = Circle(
+                     color = BLUE,
+                     radius = 1.2
+            )
+
+        Rueda2 = Circle(
+                     color = BLUE,
+                     radius = 1
+            )
+
+        Rueda1.set_fill(BLUE, opacity=0.3)
+        Rueda2.set_fill(BLUE, opacity=0.3)
+        
+        Rueda1.move_to(1.5*DOWN + 2.6*LEFT)
+        Rueda2.move_to(1.6*DOWN + 1.3*RIGHT)
+
+        Linea_horizontal = Line( start=LEFT,end=0.5*RIGHT )
+        Linea_horizontal.set_color( RED )
+
+        Linea1 = Line( start=np.array([-1, 0, 0]),end=np.array([-0.7, -1.5, 0]) ) 
+        Linea1.set_color( RED )
+
+        Linea2 = Line( start=LEFT,end=1.3*DOWN + 2.3*LEFT ) 
+        Linea2.set_color( RED )
+
+        Linea3 = Line( start=1.5*DOWN + 0.7*LEFT,end=1.3*DOWN + 2.3*LEFT ) 
+        Linea3.set_color( RED )
+
+        Linea4 = Line( start=0.5*RIGHT,end=0.4*DOWN + 0.65*RIGHT ) 
+        Linea4.set_color( RED )
+
+        Linea5 = Line( start=0.4*DOWN + 0.65*RIGHT,end=1.5*DOWN + 0.7*LEFT ) 
+        Linea5.set_color( RED )
+
+        Linea6 = Line( start=0.4*UP + 0.35*RIGHT,end= 1.4*DOWN + 1.025*RIGHT )
+        Linea6.set_color( RED )
+
+        Lineas = VGroup( Linea_horizontal, Linea1, Linea2, Linea3, Linea4, Linea5, Linea6 )
+        Lineas.scale(1.2)
+
+        Bici = VGroup( Rueda1, Rueda2, Lineas)
+
+        Bici.rotate(PI/2, axis = RIGHT)
+
+        self.play(ShowCreation(axes),run_time = 2)
+        self.play(ShowCreation(Bici),run_time = 2)
+        self.wait(2)
 
         Angulo_phi = PI/2
         Angulo_arco_bici_trasera = PI/2
         
-        Delta_Angulo = -PI/180
-        Punto_referencia_bici_trasera = Rueda_bici_trasera.get_center()+0.6*DOWN
+        Delta_Angulo = -PI/3
+        Punto_referencia_bici_trasera = Bici.get_center() + 1.5*IN
         Contador_fotograma = 0
 
         Angulo_bici_trasera = DecimalNumber(0, num_decimal_places=2, include_sign=False)
         Angulo_bici_trasera.add_updater(lambda d: d.set_value( Angulo_arco_bici_trasera * 180 / PI) )
-        Angulo_bici_trasera.next_to(Silla_bici_trasera, UP, buff=1)
+        Angulo_bici_trasera.next_to(Linea6, UP, buff=1)
 
         while Contador_fotograma <= -( (1 / Delta_Angulo) * PI ):
 
-            Bici_trasera.rotate( Angulo_phi , about_point = Punto_referencia_bici_trasera )
+            Bici.rotate( Angulo_phi , axis = RIGHT, about_point = Punto_referencia_bici_trasera)
             self.wait(0.1)
             Angulo_phi = Delta_Angulo
             Contador_fotograma = Contador_fotograma + 1
 
-            Angulo_bici_trasera.add_updater( lambda d: d.set_value( Angulo_arco_bici_trasera * 180 / PI ) )
+            Angulo_bici_trasera.add_updater( lambda d: d.set_value( (Angulo_arco_bici_trasera * 180 / PI) + 1) )
             
             Arco_bici_trasera = Arc( arc_center = Punto_referencia_bici_trasera , radius = 3, start_angle = PI/2, angle = Angulo_arco_bici_trasera )
 
@@ -99,23 +155,3 @@ class Anim_1_1(GraphScene):
             self.play( FadeOut( Angulo_bici_trasera ), FadeOut( Arco_bici_trasera ), run_time = 0.1)
     
         self.wait(2)
-
-    def setup_axes1(self):
-        GraphScene.setup_axes(self)
-        self.x_axis.set_stroke(width=2)
-        self.y_axis.set_stroke(width=2)
-        self.x_axis.set_color(WHITE)
-        self.y_axis.set_color(WHITE)
-        self.x_axis.scale(0.5)
-        self.y_axis.scale(0.8)
-        self.x_axis.move_to(2.7*DOWN)
-        self.y_axis.move_to(0.3*DOWN)
-        self.play(
-            *[Write(objeto)
-            for objeto in [
-                    self.y_axis,
-                    self.x_axis,
-                ]
-            ],
-            run_time=0.1
-        )
