95fe5ce7761ea8c65cf3eb31e61f0b7ab9b6d4cf
lmcastanedame <lmcastanedame@unal.edu.co>
2021-10-15 17:33:33 -0500

    Update Bici.py

--- Bici.py
+++ Bici.py
@@ -8,16 +8,28 @@ class Anim_1_1(ThreeDScene):
     
     def construct(self):
         axis_config = {
-            "y_max" : 4,
-            "y_min" : -4,
-            "x_max" : 5,
-            "x_min" : -4,
+            "y_max" : 0,
+            "y_min" : 0.0001,
+            "x_max" : 4,
+            "x_min" : 0,
+            "z_max" : 3,
+            "z_min" : 0,
             'color' : GREEN,
             }
         
         axes = ThreeDAxes(**axis_config)
         self.set_camera_orientation(phi = 80*DEGREES, theta = -40*DEGREES, distance = 6)
-        axes.set_color(BLUE)
+        axes.set_color(GREEN)
+
+        piso = Rectangle(
+                     color = "#C1BEBE", 
+                     height = 3,
+                     width = 12,
+            )
+        piso.set_opacity(0)
+        piso.set_fill("#C1BEBE", opacity=0.2)
+        self.add(piso)
+        piso.shift(1.5*DOWN + LEFT)
 
         cont = 0
         Lineas = []
@@ -25,7 +37,6 @@ class Anim_1_1(ThreeDScene):
         while cont <= 24:
             Linea = Line( LEFT , 0.5*LEFT )
             Linea.shift((cont-8)*0.8*LEFT + 1.5*DOWN)
-            Linea.rotate(PI/2, axis = RIGHT)
             Lineas.extend(Linea)
             cont = cont + 1
 
@@ -43,7 +54,6 @@ class Anim_1_1(ThreeDScene):
         while cont <= 24:
             Linea = Line( 0.5*LEFT , 0*LEFT )
             Linea.shift((cont-8)*0.8*LEFT + 1.5*DOWN)
-            Linea.rotate(PI/2, axis = RIGHT)
             Lineas.extend(Linea)
             cont = cont + 1
         
@@ -61,7 +71,6 @@ class Anim_1_1(ThreeDScene):
         while cont <= 24:
             Linea = Line( 0*LEFT , 0.5*RIGHT )
             Linea.shift((cont-8)*0.8*LEFT + 1.5*DOWN)
-            Linea.rotate(PI/2, axis = RIGHT)
             Lineas.extend(Linea)
             cont = cont + 1
         
@@ -79,7 +88,6 @@ class Anim_1_1(ThreeDScene):
         while cont <= 24:
             Linea = Line( 0.5*RIGHT , 1*RIGHT )
             Linea.shift((cont-8)*0.8*LEFT + 1.5*DOWN)
-            Linea.rotate(PI/2, axis = RIGHT)
             Lineas.extend(Linea)
             cont = cont + 1
         
@@ -97,7 +105,6 @@ class Anim_1_1(ThreeDScene):
         while cont <= 24:
             Linea = Line( 1*RIGHT , 1.5*RIGHT )
             Linea.shift((cont-8)*0.8*LEFT + 1.5*DOWN)
-            Linea.rotate(PI/2, axis = RIGHT)
             Lineas.extend(Linea)
             cont = cont + 1
         
@@ -136,8 +143,8 @@ class Anim_1_2(ThreeDScene):
                      radius = 1
             )
 
-        Rueda1.set_fill(BLUE, opacity=0.3)
-        Rueda2.set_fill(BLUE, opacity=0.3)
+        Rueda1.set_fill(BLUE, opacity=0.4)
+        Rueda2.set_fill(BLUE, opacity=0.4)
         
         Rueda1.move_to(1.5*DOWN + 2.6*LEFT)
         Rueda2.move_to(1.6*DOWN + 1.3*RIGHT)
@@ -145,7 +152,7 @@ class Anim_1_2(ThreeDScene):
         Linea_horizontal = Line( start=LEFT,end=0.5*RIGHT )
         Linea_horizontal.set_color( RED )
 
-        Linea1 = Line( start=np.array([-1, 0, 0]),end=np.array([-0.7, -1.5, 0]) ) 
+        Linea1 = Line( start=np.array([-1.05, 0.25, 0]),end=np.array([-0.7, -1.5, 0]) ) 
         Linea1.set_color( RED )
 
         Linea2 = Line( start=LEFT,end=1.3*DOWN + 2.3*LEFT ) 
@@ -166,7 +173,45 @@ class Anim_1_2(ThreeDScene):
         Lineas = VGroup( Linea_horizontal, Linea1, Linea2, Linea3, Linea4, Linea5, Linea6 )
         Lineas.scale(1.2)
 
-        Bici = VGroup( Rueda1, Rueda2, Lineas)
+        Silla = RoundedRectangle(
+                 color = "#8AA8FF",
+                 height = 0.2,
+                 width = 0.7,
+                 corner_radius = 0.07
+        )
+        
+        Silla.shift(0.5*UP - 1*RIGHT)
+        Silla.set_fill("#8AA8FF", opacity = 0.3)
+
+        Manubrio_1 = RoundedRectangle(
+                 color = "#8AA8FF",
+                 height = 0.2,
+                 width = 0.7,
+                 corner_radius = 0.07
+        )
+
+        Manubrio_1.shift(1*IN)
+        Manubrio_1.set_fill("#8AA8FF", opacity = 0.3)
+
+        Manubrio_2 = RoundedRectangle(
+                 color = "#8AA8FF",
+                 height = 0.2,
+                 width = 0.7,
+                 corner_radius = 0.07
+        )
+
+        Manubrio_2.shift(1*OUT)
+        Manubrio_2.set_fill("#8AA8FF", opacity = 0.3)
+        
+        Linea_Manubrio = Line( start=Manubrio_1.get_center(), end=Manubrio_2.get_center(), stroke_width=10 ) 
+        Linea_Manubrio.set_color( "#8AA8FF" )
+
+        Manubrio = VGroup(Manubrio_1, Manubrio_2, Linea_Manubrio)
+
+        Manubrio.shift(0.6*UP + 0.5*RIGHT)
+
+        Bici = VGroup(Rueda1, Rueda2, Lineas, Silla, Manubrio)
+        Bici.shift(3*UP)
 
         Bici.rotate(PI/2, axis = RIGHT)
 
@@ -180,8 +225,6 @@ class Anim_1_2(ThreeDScene):
 
         Punto_referencia_bici_trasera = Bici.get_center() + 1.5*IN
         Contador_fotograma = 0
-
-        
 
         while Contador_fotograma <= -( (1 / Delta_Angulo) * PI ):
 
